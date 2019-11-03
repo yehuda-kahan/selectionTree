e1e7be60332d2df319bdc2f4f64e38b2e400c183
yehuda kahan <yehudajka@gmail.com>
2019-11-03 13:09:44 +0200

    add

--- selectionTree/Tree.cpp
+++ selectionTree/Tree.cpp
@@ -1,22 +1,44 @@
 #include "Tree.h"
 #include<iterator>
 
-void Tree::deleteTree(DecisionTreeNode* root)
+void Tree::clear(DecisionTreeNode* root) const
 {
 	if (root == nullptr) return;
 	if (root->_isLeaf == true) delete root;
 	//list<ValidAnswer*>::iterator it;
 	for (auto it = root->_answersList.begin(); it != root->_answersList.end(); ++it)
-		deleteTree((Tree::DecisionTreeNode*)(*it)->_son);
+		clear((DecisionTreeNode*)(*it)->_son);
+	delete root;
 }
 
-void Tree::createTree(string val)
+void Tree::addRoot(string val)
 {
 	_root = new DecisionTreeNode();
 	_root->_value = val;
 }
 
-Tree::DecisionTreeNode* Tree::findQestion(string val)
+DecisionTreeNode* Tree::findQestion(string val, DecisionTreeNode* node, DecisionTreeNode* &father)
 {
+	//if (!node || (node->_isLeaf && node->_value != val)) //no node or this is not leaf and this is not the value
+	//	return nullptr;
+	if (node->_value == val)
+		return node;
+	DecisionTreeNode* temp = nullptr;
+	DecisionTreeNode* theReturn = nullptr;
+	for (auto it = node->_answersList.begin(); it != node->_answersList.end(); ++it)
+	{
+		temp = findQestion(val, (DecisionTreeNode*)(*it)->_son, father);
+		if (temp)
+		{
+			theReturn = temp;
+			father = node;
+		}
+	}
+	return theReturn;
+}
 
+bool Tree::addSon(string father, string val, string sol)
+{
+	DecisionTreeNode* theFather;
+	DecisionTreeNode* node = findQestion(father, _root, theFather);
 }
